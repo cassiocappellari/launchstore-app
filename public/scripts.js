04b47f6ddc9791aa46c2cb3832052a70a017196c
Cássio <cassiocappellari@gmail.com>
2020-08-25 16:59:23 -0300

    form value updated

---
+++ public/scripts.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('input[name="price"]')
+
+input.addEventListener('keydown', function(event) {
+    setTimeout(function() {
+        let {value} = event.target
+
+        value = value.replace(/\D/g, "")
+
+        value = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(value / 100)
+
+        event.target.value = value
+    }, 1)
+})
